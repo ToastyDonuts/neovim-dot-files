39f7b80c148bf8cbecb8ffbf8a24e96bd7a81341
Supaset Kasemsap <supaseth2008@gmail.com>
2024-06-23 21:54:27 +0700

    add toggleterm
    Added toggleterm.nvim plugin for terminal and edit gitignore to ignore
    lazy-lock

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    {
+      "<leader>to",
+      "<cmd>ToggleTerm<cr>"
+    }
+  },
+  config = true,
+  opts = {
+    direction = "float",
+  }
+}
