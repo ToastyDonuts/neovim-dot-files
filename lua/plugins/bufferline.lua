eec697608861ba7ee5a334855e629daf82bd974c
Supaset Kasemsap <supaseth2008@gmail.com>
2024-07-09 21:46:31 +0700

    fixed bufferline color
    Changed bufferline's background color to something more appealing for
    catppuccin mocha

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,24 +6,13 @@ return {
       'nvim-tree/nvim-web-devicons'
     },
     config = function ()
-      local mocha = require("catppuccin.palettes").get_palette("mocha")
-
-      vim.opt.termguicolors = true
+      local mocha = require("catppuccin.palettes").get_palette "mocha"
       require("bufferline").setup({
-        highlights = require("catppuccin.groups.integrations.bufferline").get({
-          styles = { "italic", "bold" },
-          custom = {
-            all = {
-              -- Hint
-              hint = { fg = mocha.rosewater },
-              hint_visible = { fg = mocha.rosewater },
-              hint_selected = { fg = mocha.rosewater },
-              hint_diagnostic = { fg = mocha.rosewater },
-              hint_diagnostic_visible = { fg = mocha.rosewater },
-              hint_diagnostic_selected = { fg = mocha.rosewater },
-            },
-				},
-        }),
+        highlights = {
+          fill = {
+            bg = mocha.mantle,
+          }
+        },
         options = {
           offsets = {
             {
